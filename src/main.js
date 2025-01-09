b939667ddb4a94c7d0d4919b06537d94a90e5281
xcl1224 <gjcnsfla1224@naver.com>
2025-01-09 23:31:42 +0900

    2025.1.9

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import ElementPlus from 'element-plus'
+import App from './App.vue'
+import {router} from './route'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+const pinia = createPinia()
+app.use(router)
+app.use(pinia)
+app.use(ElementPlus)
+import  './permission.js'
+
+app.mount('#app')
