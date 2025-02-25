bec5b93d8bb9e4179727b077f0f55ef7036d003f
xcl1224 <gjcnsfla1224@naver.com>
2025-02-25 20:07:21 +0900

    2025.2.25

---
+++ src/components/SearchItem.vue
@@ -0,0 +1,23 @@
+<script setup>
+  defineOptions({
+    name: 'SearchItem',
+  })
+  defineProps({
+    label:{
+      type: String,
+      default:""
+    }
+  })
+</script>
+
+<template>
+  <el-col :span="8">
+    <el-form-item :label="label">
+        <slot/>
+    </el-form-item>
+  </el-col>
+</template>
+
+<style scoped lang="scss">
+
+</style>
--- src/pages/manager/ManagerList.vue
+++ src/pages/manager/ManagerList.vue
@@ -2,10 +2,18 @@
     defineOptions({
       name: 'ManagerList',
     })
+    import Search from "@/components/Search.vue";
+    import ListHeader from "@/components/ListHeader.vue";
 </script>
 
 <template>
-
+    <el-card shadow="always">
+      <Search/>
+      <ListHeader/>
+      <div style="display: flex;align-items: center; justify-content: center; margin: 10px 0;">
+        <el-pagination background layout="prev, pager, next" :total="1000" />
+      </div>
+    </el-card>
 </template>
 
 <style scoped lang="scss">
