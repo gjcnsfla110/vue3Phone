ae3a9fbaafcdf1ccb3c1690a3b637e74d9c4df62
xcl1224 <gjcnsfla1224@naver.com>
2025-05-04 18:46:55 +0900

    1.1

--- src/pages/goods/Goods.vue
+++ src/pages/goods/Goods.vue
@@ -7,16 +7,47 @@
     import ListHeader from "@/components/ListHeader.vue";
     import Search from "@/components/Search.vue";
     import {useInitTable,useInitFrom} from "@/composables/useCommon.js";
+    import Drawer from "@/components/Drawer.vue";
+    const {
+      searchForm,
+      resetSearchForm,
+      dataList,
+      loading,
+      currentPage,
+      total,
+      limit,
+      getData,
+      handleStatusChange,
+      handleDelete,
+    } = useInitTable({
 
+    });
+
+    const {
+      editId,
+      formDrawerRef,
+      formRef,
+      formData,
+      formRules,
+      formTitle,
+      handleCreate,
+      handleUpdate,
+      resetFormData,
+      handleSubmit
+    } = useInitFrom({
+
+    });
 </script>
 <template>
     <el-card>
+       <div style="margin-bottom: 20px">
+         <el-button type="primary" size="large">添加商品</el-button>
+       </div>
        <Search backColor="rgb(248,248,248)">
           <search-item>
               <el-input></el-input>
           </search-item>
        </Search>
-       <ListHeader></ListHeader>
        <el-table
        >
           <el-table-column></el-table-column>
@@ -25,6 +56,9 @@
         <el-pagination background layout="prev, pager, next" v-model:page-size="limit" v-model:current-page="currentPage" :total="total" />
       </div>
     </el-card>
+    <Drawer  ref="drawerRef"  :title="formTitle">
+
+    </Drawer>
 </template>
 
 <style scoped lang="scss">
