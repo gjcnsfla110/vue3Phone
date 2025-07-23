3db9b1c4a5605a33d85ed2a4c90d46b9f83075a3
xcl1224 <gjcnsfla1224@naver.com>
2025-07-23 18:56:41 +0900

    2025-7-25

--- src/pages/main/Component.vue
+++ src/pages/main/Component.vue
@@ -7,6 +7,7 @@ import {useInitTable,useInitFrom} from "@/composables/useCommon.js";
 import {getComponentList,createComponent,updateComponent,deleteComponent,updateStatus} from "@/api/main/component.js";
 import Drawer from "@/components/Drawer.vue";
 import {ref} from "vue";
+import CheckImg from "@/components/CheckImg.vue";
 
 //컴포넌트이름/속하는페이지
 const pages = ref([]);
@@ -30,7 +31,7 @@ const {
       pages.value = res.pages;
       componentNames.value = res.componentNames;
       total.value = res.total;
-      dataList.value = res.dataList;
+      dataList.value = res.list;
   },
   getList:getComponentList,
   updateStatus:updateStatus,
@@ -53,7 +54,7 @@ const {
     title1:"",
     content_title:"",
     content_title1:"",
-    more:"",
+    more:0,
     more_link:"",
     list:[],
     list1:[],
@@ -73,6 +74,11 @@ const {
       required:true,
       message:"컴포넌트를 선택해주세요",
       trigger:"change"
+    },
+    title:{
+      required:true,
+      message:"타이틀을 입력해주세요",
+      trigger:"blur"
     }
   },
   create:createComponent,
@@ -92,9 +98,9 @@ getData();
     </Search>
     <el-table
     v-loading="loading"
-    :data="[]"
+    :data="dataList"
     >
-       <el-table-column>
+       <el-table-column label="타이틀" prop="title" width="300">
        </el-table-column>
     </el-table>
     <div class="pagination">
@@ -127,14 +133,87 @@ getData();
             />
           </el-select>
         </el-form-item>
-        <el-form-item>
-
+        <el-form-item label="타이틀" prop="title">
+          <el-input
+              v-model="formData.title"
+              maxlength="30"
+              style="width: 450px"
+              placeholder="타이틀을 입력하세요"
+              show-word-limit
+              type="textarea"
+          />
         </el-form-item>
-        <el-form-item>
-
+        <el-form-item label="작은타이틀" >
+          <el-input
+              v-model="formData.title1"
+              maxlength="30"
+              style="width: 450px"
+              placeholder="작은타이틀을 입력하세요"
+              show-word-limit
+              type="textarea"
+          />
         </el-form-item>
-        <el-form-item>
-
+        <el-form-item label="내용제목">
+          <el-input
+              v-model="formData.content_title"
+              :rows="5"
+              maxlength="300"
+              style="width: 450px"
+              placeholder="내용제목을 입력하세요"
+              show-word-limit
+              type="textarea"
+          />
+        </el-form-item>
+        <el-form-item label="내용부제목">
+          <el-input
+              v-model="formData.content_title1"
+              maxlength="300"
+              :rows="5"
+              style="width: 450px"
+              placeholder="내용 부제목을 입력하세요"
+              show-word-limit
+              type="textarea"
+          />
+        </el-form-item>
+        <el-form-item label="더보기">
+          <el-radio-group v-model="formData.more" size="large">
+            <el-radio-button label="숨김" :value="0" />
+            <el-radio-button label="정상" :value="1" />
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="더보기링크">
+          <el-input
+              v-model="formData.more_link"
+              maxlength="300"
+              style="width: 450px"
+              placeholder="더보기 링크를 입력하세요"
+              show-word-limit
+              type="textarea"
+          />
+        </el-form-item>
+        <el-form-item label="큰이미지">
+           <CheckImg v-model="formData.img"></CheckImg>
+        </el-form-item>
+        <el-form-item label="탑이미지">
+          <CheckImg v-model="formData.top_img"></CheckImg>
+        </el-form-item>
+        <el-form-item label="배너아이템">
+          <el-radio-group v-model="formData.banner" size="large">
+            <el-radio-button label="아님" :value="0" />
+            <el-radio-button label="맞음" :value="1" />
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="아이템수">
+          <el-input v-model="formData.item_size" style="width: 180px" placeholder="아이템수를 입력하세요"></el-input>
+        </el-form-item>
+        <el-form-item label="상태">
+          <el-radio-group v-model="formData.status" size="large">
+            <el-radio-button label="아님" :value="0" />
+            <el-radio-button label="맞음" :value="1" />
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="우선순위">
+          <el-input-number v-model="formData.ranking"/>
         </el-form-item>
       </el-form>
   </Drawer>
